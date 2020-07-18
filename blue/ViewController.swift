34145b4ab2cb81df91d8946d20fab55b71d6a191
Earth <earthpat@umich.edu>
2020-07-18 14:33:33 -0400

    Initial Commit

---
+++ blue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  blue
+//
+//  Created by Earth Patel on 7/18/20.
+//  Copyright © 2020 Earth Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
