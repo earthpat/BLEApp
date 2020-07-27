d0ec555c5431e537a87454fce692917bfabe6b2d
Earth <earthpat@umich.edu>
2020-07-26 22:06:14 -0400

    BLE Working

--- blue/ViewController.swift
+++ blue/ViewController.swift
@@ -7,14 +7,71 @@
 //
 
 import UIKit
+import CoreBluetooth
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CBCentralManagerDelegate, CBPeripheralDelegate {
+    var centralManager: CBCentralManager!
+    var myPeripheral: CBPeripheral!
+    var On:Bool!
+    
+    @IBOutlet weak var State: UILabel!
+    @IBAction func ButtonConnect(_ sender: Any) {
+        On = true
+    }
+    
+    @IBAction func ButtonDisconnect(_ sender: Any) {
+        On = false
+        centralManager.cancelPeripheralConnection(myPeripheral)
+        State.text = "Disconnected from BLE"
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == CBManagerState.poweredOn {
+            print("BLE powered on")
+            // Turned on
+            central.scanForPeripherals(withServices: nil, options: nil)
+        }
+        else {
+            print("Something wrong with BLE")
+            // Not on, but can have different issues
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        /* allows us to see what available BLE devices that we can connect to are.
+         if we want our app to automatically connect to one device we need to find the BLE module's name and make
+         pname equal to that name in the if statement.
+        */
+        if On == true{
+            if let pname = peripheral.name{
+                // change this to the name of the BLE module
+                if pname == "Earth’s MacBook Pro" {
+                    self.centralManager.stopScan()
+                    
+                    self.myPeripheral = peripheral
+                    self.myPeripheral.delegate = self
+                
+                    self.centralManager.connect(peripheral, options: nil)
+                    State.text = "Connected to: " + pname
+                }
+                print(pname)
+            }
+        }
+        else{
+            State.text = "Not Connected to anything."
+        }
+    }
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        self.myPeripheral.discoverServices(nil)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        centralManager = CBCentralManager(delegate: self, queue: nil)
     }
-
 
 }
 
